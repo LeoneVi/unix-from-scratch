db7bc1b4e030c806150c6232bcf3aab2918ed424
LeoneVi <121581270+LeoneVi@users.noreply.github.com>
2026-02-24 20:04:43 -0500

    fixed smalll formatting mistake

--- src/tools/cat.c
+++ src/tools/cat.c
@@ -17,8 +17,7 @@ int main(int argc, char *argv[]) {
         write(2, "cat: ", sizeof("cat: "));
         /* t = argv[1] copies the pointer value of argv[1] to t
          * *t will check if current character is null
-         * t++ moves char forward one byte in memory
-         */
+         * t++ moves char forward one byte in memory */
         for(t = argv[1]; *t != '\0'; t++ ){
             write(2, t, 1);
         }
