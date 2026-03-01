7f6cc7207911fe6d48fa1bdf4b9aa7ec83f8a26a
LeoneVi <121581270+LeoneVi@users.noreply.github.com>
2026-02-28 19:50:30 -0500

    fixed spacing issue

--- src/tools/echo.c
+++ src/tools/echo.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[]) {
         for(t = argv[i]; *t != '\0'; t++){
             write(1, t, 1);
         }
+        write(1, " ", sizeof(" "));
     }
     write(1, "\n", 1);
 
