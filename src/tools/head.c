7b9909cb02881a01e8c41bf3780977c3ad8238dc
LeoneVi <121581270+LeoneVi@users.noreply.github.com>
2026-02-28 20:57:44 -0500

    moved everything into switch statements

--- src/tools/head.c
+++ src/tools/head.c
@@ -35,31 +35,44 @@ void printLines(char *file, int count){
     write(1, "\n", sizeof("\n"));
 }
 
+int isCommand(char *arg){
+    if(strcmp(arg, "-n") == 0 || strcmp(arg, "-c") == 0){
+        return 1;
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[]){
 
-    if(argc < 2){
-        fprintf(stderr, "Basic usage: head [-n count | -c bytes] [file ...]\n");
-        exit(1);
-    }
-
-    char *arg1 = (char *)malloc(sizeof(argv[1]));
-    strcpy(arg1, argv[1]);
-    if(strcmp(arg1, "-n") == 0 ||
-       strcmp(arg1, "-c") == 0) // if user used command
-    {
-
-
-    } else {
-        if(arg1[0] == '-' && strlen(arg1) > 1){ // if user enters command that does not exist
-            arg1++;
-            fprintf(stderr, "head: invalid option -- %s\nusage: head [-n lines | -c bytes] [file ...]\n", arg1);
-            free(arg1);
+    switch(argc){
+        case 0:
+        case 1:
+            fprintf(stderr, "Basic usage: head [-n count | -c bytes] [file ...]\n");
             exit(1);
-        }
+        case 2:
+            char *arg1 = (char *)malloc(sizeof(argv[1]));
+            strcpy(arg1, argv[1]);
 
-        // When user only inputs a file, head prints the first 10 lines.
-        printLines(argv[1], 10);
+            if(isCommand(arg1)){
+                if(argc == 2){
+                    arg1++;
+                    fprintf(stderr, "head: option requires an argument -- %s\n"
+                                    "usage: head [-n lines | -c bytes] [file ...]\n", arg1);
+                    free(arg1);
+                    exit(1);
+                }
+
+            } else {
+                if(arg1[0] == '-' && strlen(arg1) > 1){ // if user enters command that does not exist
+                    arg1++;
+                    fprintf(stderr, "head: invalid option -- %s\n"
+                                    "usage: head [-n lines | -c bytes] [file ...]\n", arg1);
+                    free(arg1);
+                    exit(1);
+                }
+
+                // When user only inputs a file, head prints the first 10 lines.
+                printLines(argv[1], 10);
+            }
     }
-
-
 }
